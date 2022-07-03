4cf81923405d4684a86014a0890af2048d798c16
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-07-03 20:11:32 +0200

    chore: get labels and correct type

--- PlayersData/utils.py
+++ PlayersData/utils.py
@@ -1,26 +1,23 @@
-from typing import Union, Optional
+from typing import Union
 
 import sc2.unit
 from sc2.position import Point2
 from sc2.ids.upgrade_id import UpgradeId
 from sc2.ids.unit_typeid import UnitTypeId
 
-from PlayersData.constants import unit_alias, unit_label, structure_label, upgrade_label
+from PlayersData.labels import alias, correct_alias
 
 
-def get_label(u: Union[UnitTypeId, UpgradeId, sc2.unit.Unit], /):
+def get_label(u, /) -> int:
     if isinstance(u, sc2.unit.Unit):
         u = u.type_id
-    if u in upgrade_label:
-        return upgrade_label[u]
-    else:
-        _type = unit_alias.get(u, u)
-        return unit_label[_type] if _type in unit_label else structure_label[_type]
+    _type = correct_alias.get(u, u)
+    return alias[_type]
 
 
 def correct_type(unit: sc2.unit.Unit):
     _type = unit.type_id
-    return unit_alias.get(_type, _type)
+    return correct_alias.get(_type, _type)
 
 
 def townhall_is_expansion(unit: sc2.unit.Unit, expansions: list[Point2]):
