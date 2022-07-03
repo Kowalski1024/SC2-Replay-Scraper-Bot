ed23ec3683f92ff52a75719cdc0cf4746b9338e4
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-07-03 17:37:00 +0200

    feat: ObserverBot class implementation

--- observer_bot.py
+++ observer_bot.py
@@ -1,6 +1,7 @@
 import csv
+
 from sc2.observer_ai import ObserverAI
-from collections import Counter
+
 from PlayersData.players_data import PlayersData
 
 
@@ -8,8 +9,9 @@ class ObserverBot(ObserverAI):
     def __init__(self, file_name: str):
         super().__init__()
         self._file_name = file_name
+        self.file = open('data/datasets/'+self._file_name+'.csv', 'a', encoding='UTF8', newline='')
+        self.writer = csv.writer(self.file)
         self.iteration = 0
-        self._prev_abilities = Counter()
         self.players_data = PlayersData(self)
 
     async def on_unit_destroyed(self, unit_tag: int):
@@ -18,20 +20,19 @@ class ObserverBot(ObserverAI):
     async def on_start(self):
         print("Game started")
 
-    def on_prepare_step(self):
+    def before_step(self):
         self.players_data.on_prepare_step()
 
     def after_step(self):
         self.players_data.after_step()
 
     async def on_step(self, iteration):
-        self.on_prepare_step()
+        self.before_step()
         self.iteration = iteration
         if self.players_data.new_train_orders:
-            with open('data/datasets/'+self._file_name+'.csv', 'a', encoding='UTF8', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(self.players_data.train_data)
+            self.writer.writerow(self.players_data.train_data)
         self.after_step()
 
     def on_end(self, result):
+        self.file.close()
         print("Game ended.")
