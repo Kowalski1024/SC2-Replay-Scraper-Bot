bd8f5b6a860c0d1230a1d600144f9ef90943424b
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-07-03 14:57:43 +0200

    feat: move observer_ai.py to root

--- PlayersData/players_data.py
+++ PlayersData/players_data.py
@@ -4,7 +4,7 @@ from collections import Counter
 from sc2.ids.upgrade_id import UpgradeId
 from sc2.bot_ai import BotAI
 from sc2.unit import Unit
-from sc2.observer_ai import ObserverAI
+from observer_ai import ObserverAI
 from sc2.position import Point2
 from sc2.ids.unit_typeid import UnitTypeId
 
--- observer_bot.py
+++ observer_bot.py
@@ -1,5 +1,5 @@
 import csv
-from sc2.observer_ai import ObserverAI
+from observer_ai import ObserverAI
 from sc2.units import Unit, Units
 from collections import Counter
 from sc2.cache import property_cache_once_per_frame
@@ -19,23 +19,11 @@ class ObserverBot(ObserverAI):
         self.iteration = 0
         self._prev_abilities = Counter()
         self.players_data = PlayersData(self)
-        self._start_location = Point2()
-
-    @property
-    def start_location(self) -> Point2:
-        return self._start_location
 
     async def on_unit_destroyed(self, unit_tag: int):
         await self.players_data.on_unit_destroyed(unit_tag)
 
-    def correct(self):
-        self.correct_data()
-        self.correct_units()
-
     async def on_start(self):
-        self.correct()
-        self._start_location = self.structures(UnitTypeId.NEXUS).first.position
-        # pprint(vars(self.state.common))
         print("Game started")
 
     def on_prepare_step(self):
@@ -44,32 +32,7 @@ class ObserverBot(ObserverAI):
     def after_step(self):
         self.players_data.after_step()
 
-    def correct_data(self):
-        common = self.state.common
-        for attr in common.ATTRIBUTES:
-            if hasattr(self, attr):
-                setattr(self, attr, getattr(common, attr))
-
-    def correct_units(self):
-        self.all_units = self.units
-        self.units = Units([], self)
-        self.enemy_units = Units([], self)
-        self.structures = Units([], self)
-        self.enemy_structures = Units([], self)
-        for unit in self.all_units:
-            if unit.alliance == 1:
-                if unit.is_structure:
-                    self.structures.append(unit)
-                else:
-                    self.units.append(unit)
-            elif unit.alliance == 4:
-                if unit.is_structure:
-                    self.enemy_structures.append(unit)
-                else:
-                    self.enemy_units.append(unit)
-
     async def on_step(self, iteration):
-        self.correct()
         self.on_prepare_step()
         self.iteration = iteration
         if self.players_data.new_train_orders:
