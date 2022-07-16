a65043eab512531e2aa30c4bb06191ee0466f8ff
Kowalski1024 <tomasz_wojnar@hotmail.com>
2022-07-16 13:12:16 +0200

    chore: change unit memorisation

--- PlayersData/Races/base.py
+++ PlayersData/Races/base.py
@@ -5,29 +5,21 @@ from sc2.bot_ai import BotAI
 from sc2.observer_ai import ObserverAI
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.units import Units
+from sc2.unit import Unit
 from sc2.position import Point2
 from sc2.data import race_townhalls, Race
 
 from PlayersData.data_structures.expiring_dict import ExpiringDict
 from PlayersData.labels import Labels
-from PlayersData.unit import Unit
 from PlayersData.cache import property_cache_once_per_frame
 
 
 class PlayerData:
     def __init__(self, bot_ai: Union[BotAI, ObserverAI]):
         self._bot = bot_ai
-        self._units: Optional[dict] = None
-        self._unit_types: DefaultDict[UnitTypeId, set[int]] = defaultdict(set)
+        self._seen_units: dict[int, UnitTypeId] = dict()
         self._data_dict = {label.value: 0 for label in Labels}
         self._upgrades_set = set()
-
-    @property_cache_once_per_frame
-    def units(self) -> Units:
-        return Units(self._units.values(), self._bot)
-
-    def has_unit(self, tag: int):
-        return tag in self._units
 
     @property
     def structures(self) -> Units:
@@ -37,6 +29,9 @@ class PlayerData:
     def data_dict(self):
         self.update_structures()
         return self._data_dict.copy()
+
+    def has_unit(self, tag: int):
+        return tag in self._seen_units
 
     def update(self):
         pass
@@ -58,33 +53,28 @@ class PlayerData:
                 self._data_dict[Labels.EXPANSIONS.value] += 1
         self._data_dict.update(structures)
 
-    @property
-    def upgrades_set(self):
-        return self._upgrades_set
-
 
 class EnemyData(PlayerData):
     def __init__(self, bot_ai):
         super().__init__(bot_ai)
         self._units: ExpiringDict[int, Unit] = ExpiringDict(self._bot, 1000)
-        self._seen_units: dict[int, Unit] = dict()
 
     @property
     def visible_units(self) -> Units:
         return self._bot.enemy_units
 
+    @property_cache_once_per_frame
+    def units(self) -> Units:
+        return Units(self._units.values(), self._bot)
+
     @property
     def structures(self) -> Units:
         return self._bot.enemy_structures
-
-    def has_unit(self, tag: int):
-        return tag in self._seen_units
 
 
 class AllianceData(PlayerData):
     def __init__(self, bot_ai):
         super().__init__(bot_ai)
-        self._units: dict[int, Unit] = {}
         self.prev_state = None
 
     @property
--- PlayersData/Races/protoss.py
+++ PlayersData/Races/protoss.py
@@ -18,56 +18,15 @@ class AllianceProtoss(AllianceData):
     def update(self):
         for unit in self._bot.units:
             tag = unit.tag
-            if tag in self._units:
-                self._units[tag].update(unit)
-            else:
+            if tag not in self._seen_units:
                 unit_type = correct_type(unit)
-                self._unit_types[unit_type].add(tag)
-                self._units[tag] = Unit(unit)
                 if unit_type in alias:
                     self._data_dict[Labels.get_value(unit_type)] += 1
-
-    # TODO: self.state.upgrades
-    # def on_upgrade_complete(self, upgrade: UpgradeId):
-    #     if upgrade in {
-    #         UpgradeId.PROTOSSSHIELDSLEVEL1,
-    #         UpgradeId.PROTOSSSHIELDSLEVEL2,
-    #         UpgradeId.PROTOSSSHIELDSLEVEL3
-    #     }:
-    #         self._upgrades_vector[4] = upgrade.value - UpgradeId.PROTOSSSHIELDSLEVEL1.value + 1
-    #     elif upgrade in {
-    #         UpgradeId.PROTOSSAIRARMORSLEVEL1,
-    #         UpgradeId.PROTOSSAIRARMORSLEVEL2,
-    #         UpgradeId.PROTOSSAIRARMORSLEVEL3
-    #     }:
-    #         self._upgrades_vector[3] = upgrade.value - UpgradeId.PROTOSSAIRARMORSLEVEL1.value + 1
-    #     elif upgrade in {
-    #         UpgradeId.PROTOSSAIRWEAPONSLEVEL1,
-    #         UpgradeId.PROTOSSAIRWEAPONSLEVEL2,
-    #         UpgradeId.PROTOSSAIRWEAPONSLEVEL3
-    #     }:
-    #         self._upgrades_vector[2] = upgrade.value - UpgradeId.PROTOSSAIRWEAPONSLEVEL1.value + 1
-    #     elif upgrade in {
-    #         UpgradeId.PROTOSSGROUNDARMORSLEVEL1,
-    #         UpgradeId.PROTOSSGROUNDARMORSLEVEL2,
-    #         UpgradeId.PROTOSSGROUNDARMORSLEVEL3
-    #     }:
-    #         self._upgrades_vector[1] = upgrade.value - UpgradeId.PROTOSSGROUNDARMORSLEVEL1.value + 1
-    #     elif upgrade in {
-    #         UpgradeId.PROTOSSGROUNDWEAPONSLEVEL1,
-    #         UpgradeId.PROTOSSGROUNDWEAPONSLEVEL2,
-    #         UpgradeId.PROTOSSGROUNDWEAPONSLEVEL3
-    #     }:
-    #         self._upgrades_vector[0] = upgrade.value - UpgradeId.PROTOSSGROUNDWEAPONSLEVEL1.value + 1
-    #     else:
-    #         self._upgrades_set.add(upgrade)
-    #         self._upgrades_vector[get_label(upgrade)] = 1
+                self._seen_units[tag] = unit_type
 
     def on_unit_destroyed(self, tag: int):
-        unit_type = correct_type(self._bot._units_previous_map[tag])
-        self._units.pop(tag)
-        self._unit_types[unit_type].remove(tag)
-
+        unit_type = self._seen_units[tag]
+        self._seen_units.pop(tag)
         if unit_type in alias:
             self._data_dict[Labels.get_value(unit_type)] -= 1
 
@@ -79,28 +38,16 @@ class EnemyProtoss(EnemyData):
     def update(self):
         for unit in self.visible_units:
             tag = unit.tag
-            if tag in self._units:
-                self._units[tag].update(unit)
-                self._units.refresh(tag)
-            else:
+            self._units[tag] = unit
+            if tag not in self._seen_units:
                 unit_type = correct_type(unit)
-                self._units[tag] = Unit(unit)
-                self._unit_types[unit_type].add(tag)
                 if unit_type in alias:
                     if tag not in self._seen_units:
                         self._data_dict[Labels.get_value(unit_type)] += 1
-                        # if unit_type == UnitTypeId.ARCHON:
-                        #     if self._vector_dict[Labels.HIGHTEMPLAR] >= 2:
-                        #         self._vector_dict[Labels.HIGHTEMPLAR] -= 2
-                        #     elif self._vector_dict[Labels.DARKTEMPLAR] >= 2:
-                        #         self._vector_dict[Labels.DARKTEMPLAR] -= 2
-            self._seen_units[tag] = unit
+                self._seen_units[tag] = unit_type
 
     def on_unit_destroyed(self, tag: int):
-        unit_type = correct_type(self._seen_units[tag])
-        self._units.pop(tag)
-        self._unit_types[unit_type].remove(tag)
-        del self._seen_units[tag]
-
+        unit_type = self._seen_units[tag]
+        self._seen_units.pop(tag)
         if unit_type in alias:
             self._data_dict[Labels.get_value(unit_type)] -= 1
